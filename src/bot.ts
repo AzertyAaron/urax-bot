c6f0bd4d822e59f5dda3f7ffce1a290f77778663
AzertyAaron <azertyaaron@gmail.com>
2018-03-09 17:55:34 -0500

    Added the apility to set the chance in the config and through a command

--- src/bot.ts
+++ src/bot.ts
@@ -3,10 +3,21 @@ const config = require('../config/config.json')
 
 const bot = new DiscordJS.Client
 
+let chance: number = config.chance
+
 bot.on('message', msg => {
   if (!msg.author.bot) {
-    if (Math.random() <= 0.1) {
-      msg.channel.send('u r a ' + msg.content.split(' ').pop())
+    if (msg.author.id === config.ownerId) {
+      if (msg.content.includes('!setChance ')) {
+        const parsedChance = msg.content.split(' ').pop()
+        chance = Number.parseInt(parsedChance)
+
+        msg.reply('Chance set to ' + chance.toString())
+      }
+    }
+
+    if (Math.random() <= chance) {
+      msg.channel.send('u r a ' + msg.content.split(' ').pop().toLowerCase())
     }
   }
 })
