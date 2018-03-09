afbd533456a29b77b8587f5c57a7121abb8a4564
AzertyAaron <azertyaaron@gmail.com>
2018-03-09 04:16:14 -0500

    wrong condition

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,7 @@ const config = require('../config/config.json')
 const bot = new DiscordJS.Client
 
 bot.on('message', msg => {
-  if (msg.author.bot) {
+  if (!msg.author.bot) {
     if (Math.random() <= 0.1) {
       msg.channel.send('u r a ' + msg.content.split(' ').pop())
     }
