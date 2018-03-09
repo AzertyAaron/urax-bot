01a03ec48efa6712ba09d228c26fdb3111221956
AzertyAaron <azertyaaron@gmail.com>
2018-03-09 04:09:11 -0500

    adding bot verification

--- src/bot.ts
+++ src/bot.ts
@@ -4,8 +4,10 @@ const config = require('../config/config.json')
 const bot = new DiscordJS.Client
 
 bot.on('message', msg => {
-  if (Math.random() >= 0.1) {
-    msg.channel.send('u r a ' + msg.content.split(' ').pop())
+  if (msg.author.bot) {
+    if (Math.random() <= 0.1) {
+      msg.channel.send('u r a ' + msg.content.split(' ').pop())
+    }
   }
 })
 
