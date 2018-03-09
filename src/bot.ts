785da79a7a4b781c0c010409dd4dc9fa980ca3b8
AzertyAaron <azertyaaron@gmail.com>
2018-03-09 04:00:42 -0500

    now the bot responds to a random message

--- src/bot.ts
+++ src/bot.ts
@@ -4,7 +4,9 @@ const config = require('../config/conifg')
 const bot = new DiscordJS.Client
 
 bot.on('message', msg => {
-
+  if (Math.random() >= 0.1) {
+    msg.channel.send('u r a ' + msg.content.split(' ').pop())
+  }
 })
 
 bot.login(config.token)
