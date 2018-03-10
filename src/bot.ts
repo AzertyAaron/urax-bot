2eb756e1aec1d67970d499c4a272c2700bf4fa0a
AzertyAaron <azertyaaron@gmail.com>
2018-03-10 07:32:27 -0500

    Using .startsWith instead of .includes because that's how it should be Also the bot now displays chance in it's status

--- src/bot.ts
+++ src/bot.ts
@@ -8,11 +8,12 @@ let chance: number = config.chance
 bot.on('message', msg => {
   if (!msg.author.bot) {
     if (msg.author.id === config.owner) {
-      if (msg.content.includes('!setChance ')) {
+      if (msg.content.startsWith('!setChance ')) {
         const parsedChance = msg.content.split(' ').pop()
         chance = Number.parseFloat(parsedChance)
 
         msg.reply('Chance set to ' + chance.toString())
+        bot.user.setPresence({ game: { name: `Chance set to ${chance}` } })
       }
     }
 
@@ -21,6 +22,10 @@ bot.on('message', msg => {
       console.log(`[TRIGGERED] ${msg.guild.name} > ${msg.channel.id} > by ${msg.author.username}`)
     }
   }
+})
+
+bot.on('ready', () => {
+  bot.user.setPresence({ game: { name: `Chance set to ${chance}` } })
 })
 
 bot.login(config.token)
