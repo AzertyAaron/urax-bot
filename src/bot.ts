47380de865a3cbb60ae5d162d6cbb99f6f8b982d
AzertyAaron <azertyaaron@gmail.com>
2018-03-12 20:57:27 -0500

    Destructuring the CLient class instead of importing the whole thing
    Also using message instead of msg since there's not reason to shorten variable names

--- src/bot.ts
+++ src/bot.ts
@@ -1,25 +1,25 @@
-import * as DiscordJS from 'discord.js'
+import { Client } from 'discord.js'
 const config = require('../config/config.json')
 
-const bot = new DiscordJS.Client
+const bot = new Client
 
 let chance: number = config.chance
 
-bot.on('message', msg => {
-  if (!msg.author.bot) {
-    if (msg.author.id === config.owner) {
-      if (msg.content.startsWith('!setChance ')) {
-        const parsedChance = msg.content.split(' ').pop()
+bot.on('message', message => {
+  if (!message.author.bot) {
+    if (message.author.id === config.owner) {
+      if (message.content.startsWith('!setChance ')) {
+        const parsedChance = message.content.split(' ').pop()
         chance = Number.parseFloat(parsedChance)
 
-        msg.reply('Chance set to ' + chance.toString())
+        message.reply('Chance set to ' + chance.toString())
         bot.user.setPresence({ game: { name: `Chance set to ${chance}` } })
       }
     }
 
     if (Math.random() <= chance) {
-      msg.channel.send('u r a ' + msg.content.split(' ').pop().toLowerCase())
-      console.log(`[TRIGGERED] ${msg.guild.name} > ${msg.channel.id} > by ${msg.author.username}`)
+      message.channel.send('u r a ' + message.content.split(' ').pop().toLowerCase())
+      console.log(`[TRIGGERED] ${message.guild.name} > ${message.channel.id} > by ${message.author.username}`)
     }
   }
 })
